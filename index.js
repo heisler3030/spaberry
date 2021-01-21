ae16924b479f1d7d64861514b75dc2c7d363c14c
Hamish Eisler <heisler3030@users.noreply.github.com>
2021-01-21 07:41:11 +0000

    first drop gpio code

--- index.js
+++ index.js
@@ -1,8 +1,57 @@
-const express = require('express');
-const app = express();
+const Express = require('express');
+const app = Express();
+const Gpio = require('onoff').Gpio;
+
+const clock = new Gpio(5, 'in', 'rising');
+const data = new Gpio(6, 'in');
+const controls = new Gpio(13, 'in');
+
+let lastClock = Date.now();
+let thisClock = Date.now();
+let clockCount = 0;
+let frameCount = 0;
+let dataFrame = "";
+let controlsFrame = "";
+let status = "";
+
+clock.watch((err, value) => {
+    if (err) {
+        throw err;     
+    }
+    thisClock = Date.now();
+    let dataBit = data.readSync();
+    let controlsBit = controls.readSync();
+    if (thisClock > lastClock + 10) {
+        newframe();
+    }
+    dataFrame += dataBit;
+    controlsFrame += controlsBit;
+    clockCount++;
+    lastClock = thisClock;
+});
+
+function newframe() {
+    status = `${dataFrame}|${controlsFrame}|${clockCount} ticks|${frameCount} frames`;
+    dataFrame = "";
+    controlsFrame = "";
+    clockCount=0;
+    frameCount++;
+    console.log(status);
+}
+
+process.on('SIGINT', _ => {
+  clock.unexport();
+  data.unexport();
+  controls.unexport();
+});
  
 app.get('/', function (req, res) {
-  res.send('Hello World');
-})
- 
+    let webStatus = status.replaceAll('|', '<br>');
+    res.send(`Status:  ${webStatus}`);
+});
+
+app.get('/temp', async function (req, res) {
+  res.send('Temperature');
+});
+
 app.listen(process.env.PORT || 3000);
