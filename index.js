a1952a9676d6d946a1dcd34c1c97961058be218b
Hamish Eisler <heisler3030@users.noreply.github.com>
2021-01-22 02:50:46 +0000

    switch to pigpio library

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 const Express = require('express');
 const app = Express();
-const Gpio = require('onoff').Gpio;
+//const Gpio = require('onoff').Gpio;
+const Gpio = require('pigpio').Gpio;
 
-const clock = new Gpio(5, 'in', 'rising');
-const data = new Gpio(6, 'in');
-const controls = new Gpio(13, 'in');
+// const clock = new Gpio(5, 'in', 'rising');
+// const data = new Gpio(6, 'in');
+// const controls = new Gpio(13, 'in');
 
-let lastClock = process.hrtime.bigint();
-let thisClock = process.hrtime.bigint();
+const clock = new Gpio(5, {mode: Gpio.INPUT, edge: Gpio.RISING});
+const data = new Gpio(6, {mode: Gpio.INPUT, edge: Gpio.RISING});
+const controls = new Gpio(13, {mode: Gpio.INPUT, edge: Gpio.RISING});
+
+//let lastClock = process.hrtime.bigint();
+let lastTick = 0;
 let clockCount = 0;
 let frameCount = 0;
 let dataFrame = "";
@@ -15,24 +20,33 @@ let controlsFrame = "";
 let status = "";
 let dataBit = "";
 
-clock.watch((err, value) => {
-    if (err) {
-        throw err;     
+// clock.watch((err, value) => {
+//     if (err) {
+//         throw err;     
+//     }
+//     let thisClock = process.hrtime.bigint();
+//     //dataBit = data.readSync();
+//     //let controlsBit = controls.readSync();
+//     if (thisClock > lastClock + BigInt(2e7)) {
+//         newframe(dataFrame, clockCount);
+//     }
+//     // dataFrame += dataBit;
+//     //controlsFrame += controlsBit;
+//     clockCount++;
+//     lastClock = thisClock;
+// });
+
+clock.on('interrupt', (level,tick) => {
+    if (tick > lastTick + 10000) { // if last tick was over 10ms ago
+        newframe(dataFrame, clockCount);
     }
-    thisClock = process.hrtime.bigint();
-    //dataBit = data.readSync();
-    //let controlsBit = controls.readSync();
-    if (thisClock > lastClock + BigInt(1e7)) {
-        newframe();
-    }
-    // dataFrame += dataBit;
-    //controlsFrame += controlsBit;
-    clockCount++;
-    lastClock = thisClock;
+    dataFrame += data.digitalRead();
+    clockCount++
+    lastTick = tick;
 });
 
-function newframe() {
-    status = `${dataFrame}|${controlsFrame}|${clockCount} ticks|${frameCount} frames`;
+function newframe(df, cc) {
+    status = `${df}|${controlsFrame}|${cc} ticks|${frameCount} frames`;
     dataFrame = "";
     controlsFrame = "";
     clockCount=0;
